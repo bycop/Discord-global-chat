64fc408e8445aae10bde84af0cea97d840d048c7
Sfournio <44951701+bycop@users.noreply.github.com>
2021-06-16 19:29:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -447,7 +447,7 @@ bot.on("message", (message) => {
         .setColor("0x992D22")
         .setAuthor("System")
         .setDescription(
-          `**You have been banned from the "Cynthia Commuity Chat"!**\nThis might have a few reasons:\n**1)** You send illegal or other unwanted content (Including but not limited to: Gore, rape, murder, ch!ldp0rn)\n**2)** You were a dick to the community!\n**3)** Users and/or moderators suggested banning you for behaviour outside of this chat\n_ _\n*To get unbanned, join the Cynthia AI Support-Server (https://discord.gg/Xnyg477hym) and open a ticket. We might or might not unban you, as we see it fit!*\n_ _\n_ _\n Sent at: ${moment().format(
+          `**You have been banned from the "Global Chat"!**\nThis might have a few reasons:\n**1)** You send illegal or other unwanted content (Including but not limited to: Gore, rape, murder, ch!ldp0rn)\n**2)** You were a dick to the community!\n**3)** Users and/or moderators suggested banning you for behaviour outside of this chat\n_ _\n*Sent at: ${moment().format(
             "MMMM Do YYYY, h:mm:ss a"
           )}`
         )
